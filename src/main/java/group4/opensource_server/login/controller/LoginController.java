4796391ada68428077e0e5cf737eca3b8749751d
leeyeongjae1 <lyj8520675@naver.com>
2025-05-14 16:58:39 +0900

    swagger

--- src/main/java/group4/opensource_server/login/controller/LoginController.java
+++ src/main/java/group4/opensource_server/login/controller/LoginController.java
@@ -24,7 +24,7 @@ public class LoginController {
     private final KakaoService kakaoService;
     private final AppleService appleService;
 
-    @Operation(summary = "카카오 로그인 처리", description = "프론트엔드에서 전달받은 카카오 인가 코드를 사용해 JWT 토큰을 발급합니다.")
+    @Operation(summary = "카카오 로그인 처리- web", description = "프론트엔드에서 전달받은 카카오 인가 코드를 사용해 JWT 토큰을 발급합니다.")
     @PostMapping("/login/kakao")
     public ResponseEntity<LoginResponseDto> kakaoLogin(@RequestBody Map<String, String> body){
         try{
@@ -33,14 +33,14 @@ public class LoginController {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Item Not Found");
         }
     }
-
-    @Operation(summary = "애플 로그인 처리", description = "프론트엔드에서 전달받은 애플 인가 코드를 사용해 JWT 토큰을 발급합니다.")
+    @Operation(summary = "카카오 로그인 처리- ios", description = "프론트엔드에서 전달받은 카카오 인가 코드를 사용해 JWT 토큰을 발급합니다.")
     @PostMapping("/login/kakao/token")
     public ResponseEntity<LoginResponseDto> loginWithAccessToken(@RequestBody Map<String, String> body) {
         LoginResponseDto response = kakaoService.kakaoLoginWithToken(body.get("accessToken"));
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "애플 로그인 처리", description = "프론트엔드에서 전달받은 애플 인가 코드를 사용해 JWT 토큰을 발급합니다.")
     @PostMapping("/login/apple")
     public ResponseEntity<LoginResponseDto> appleLogin(@RequestBody Map<String, String> body) {
         try{
