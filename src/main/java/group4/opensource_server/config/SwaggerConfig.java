c309db6af971c6d827d5cd2156034ac336bbe239
leeyeongjae1 <lyj8520675@naver.com>
2025-05-14 15:38:49 +0900

    swagger

--- src/main/java/group4/opensource_server/config/SwaggerConfig.java
+++ src/main/java/group4/opensource_server/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package group4.opensource_server.config;
 
 
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +15,14 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)))
                 .info(new Info()
                         .title("SSP Server API")
                         .version("1.0.0")
