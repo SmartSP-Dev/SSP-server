c459cb5fbcba4fa0f782013824afed687620a4a6
LEEYEONGJAE1 <lyj8520675@naver.com>
2025-05-19 18:29:57 +0900

    token

--- src/main/java/group4/opensource_server/jwt/JwtUtil.java
+++ src/main/java/group4/opensource_server/jwt/JwtUtil.java
@@ -24,7 +24,7 @@ public class JwtUtil {
         return Jwts.builder()
                 .setSubject(subject)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 ))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
